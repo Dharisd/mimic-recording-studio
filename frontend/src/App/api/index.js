5b2eed3236ab75d450ffb24e02d32b3dcd708dee
Mohamed Ishan <icernn03@gmail.com>
2025-02-10 14:09:39 +0500

    refactor: adds api login fuction and removes uuid from api calls

--- frontend/src/App/api/index.js
+++ frontend/src/App/api/index.js
@@ -1,7 +1,7 @@
-const apiRoot = "/api/";  // Changed to use relative path
+const apiRoot = "http://localhost:5000/api/";  // Changed to use relative path
 
-export const postAudio = (audio, prompt, uuid) => {
-    return fetch(apiRoot + `audio/?uuid=${uuid}&prompt=${prompt}`, {
+export const postAudio = (audio, prompt) => {
+    return fetch(apiRoot + `audio/&prompt=${prompt}`, {
         method: "POST",
         body: audio,
         headers: {
@@ -10,20 +10,20 @@ export const postAudio = (audio, prompt, uuid) => {
     })
 };
 
-export const getPrompt = uuid => {
-    return fetch(apiRoot + `prompt/?uuid=${uuid}`, {
+export const getPrompt = () => {
+    return fetch(apiRoot + `prompt`, {
         method: "GET"
     });
 };
 
-export const getUser = uuid => {
-    return fetch(apiRoot + `user/?uuid=${uuid}`, {
+export const getUser = () => {
+    return fetch(apiRoot + `user`, {
         method: 'GET'
     })
 }
 
-export const getAudioLen = (uuid, audio) => {
-    return fetch(apiRoot + `audio/?uuid=${uuid}&get_len=True`, {
+export const getAudioLen = (audio) => {
+    return fetch(apiRoot + `audio/?get_len=True`, {
         method: "POST",
         body: audio,
         headers: {
@@ -32,14 +32,40 @@ export const getAudioLen = (uuid, audio) => {
     })
 }
 
-export const createUser = (uuid, name) => {
+export const createUser = (uuid, name, password, is_admin=false) => {
     const data = {
         uuid: uuid,
-        user_name: name
+        user_name: name,
+        password: password,
+        is_admin: is_admin
     }
     return fetch(apiRoot + `user/`, {
         method: 'POST',
         body: JSON.stringify(data),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+export const loginUser = (name, password) => {
+    const data = {
+        user_name: name,
+        password: password,
+    }
+    return fetch(apiRoot + `auth/`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    })
+}
+export const logoutUser = () => {
+    return fetch(apiRoot + `auth/`, {
+        method: 'DELETE',
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"

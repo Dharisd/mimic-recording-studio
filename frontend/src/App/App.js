2724c89e33cac6b5880995f738decab3f8047538
Mohamed Ishan <icernn03@gmail.com>
2025-02-10 14:11:35 +0500

    feat: adds auth context to root

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,33 +1,48 @@
 import React, { Component } from "react";
 import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
 import Header from "./Header";
 import Intro from "./Intro";
 import Record from "./Record";
 import Tutorial from "./Tutorial";
-import { getUUID, createUUID } from "./api/localstorage";
+import Login from "./Login";
+import { getUUID } from "./api/localstorage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+const routes = [
+  { path: "/login", component: Login, isPrivate: false },
+  { path: "/", component: Intro, isPrivate: true },
+  { path: "/record", component: Record, isPrivate: true },
+  { path: "/tutorial", component: Tutorial, isPrivate: true },
+];
 
 class App extends Component {
   componentDidMount(){
     if (!getUUID()){
-      createUUID()
     }
   }
 
   render() {
     return (
-      <Router onUpdate={() => window.scrollTo(0, 0)}>
-        <div className="App">
-          <Header />
-          <div className="page">
-            <Route exact path="/" component={Intro} />
-            <Route path="/record" component={Record} />
-            <Route path="/tutorial" component={Tutorial} />
+      <AuthProvider>
+        <Router onUpdate={() => window.scrollTo(0, 0)}>
+          <div className="App">
+            <Header />
+            <div className="page">
+              <Switch>
+              {routes.map(({ path, component, isPrivate }, index) =>
+                  isPrivate ? (
+                    <PrivateRoute key={index} path={path} component={component} />
+                  ) : (
+                    <Route key={index} path={path} component={component} />
+                  )
+                )}
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AuthProvider>
     );
   }
 }

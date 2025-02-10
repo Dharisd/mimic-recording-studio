5751a30b4594ab8cbaee0ef1e74a353c858c253d
Mohamed Ishan <icernn03@gmail.com>
2025-02-10 14:11:07 +0500

    feat: adds logout button to header

--- frontend/src/App/Header.js
+++ frontend/src/App/Header.js
@@ -1,15 +1,37 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
 import logo from "./assets/logo.svg";
+import { AuthContext } from "./AuthContext";
 
 class Header extends Component {
+  static contextType = AuthContext;
+
+  handleLogout = async (e) => {
+    e.preventDefault();
+    await this.context.logout();
+  };
+
   render() {
+    const { user } = this.context;
+
+
     return (
       <div className="topnav">
         <Link to="/">
           <img className="logo" src={logo} height="40px" alt=""/>
         </Link>
+        <div className="nav-links">
+          {user ? (
+              <button onClick={this.handleLogout} className="logout-button">
+                Logout
+              </button>
+          ) : (
+            <Link to="/login" className="login-link">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     );
   }

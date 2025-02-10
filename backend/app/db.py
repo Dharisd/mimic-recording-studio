01aa339e4ab3734ca7a467573dfd28b93c7691b2
Mohamed Ishan <icernn03@gmail.com>
2025-02-10 13:54:07 +0500

    feat: adds password and admin flag to usermodel

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,17 +1,16 @@
 """The Data layer. Has anything to do with creating,
     storing, getting, udpating, data
 """
-
+import hashlib
 import os
 import datetime
 from .protocol import response
 from .audio import Audio
 from peewee import (
-    Model, SqliteDatabase, CharField, IntegerField,
+    Model, SqliteDatabase, CharField, IntegerField, BooleanField,
     DateTimeField, ForeignKeyField, DoesNotExist, FloatField,
     AutoField
 )
-
 
 # define paths and directories
 mimic_studio_dir = os.path.join(
@@ -33,6 +32,8 @@ class UserModel(Model):
     uuid = CharField(primary_key=True)
     mycroft_uuid = CharField(default="")
     user_name = CharField()
+    password_hash = CharField()
+    is_admin = BooleanField(default=False)
     prompt_num = IntegerField(default=0)
     total_time_spoken = FloatField(default=0.0)
     len_char_spoken = IntegerField(default=0)
@@ -44,16 +45,40 @@ class UserModel(Model):
         database = mimic_studio_db
 
     @staticmethod
+    def hash_password(password):
+        """Hash a password with SHA-256.
+        
+        Args:
+            password (str): The plain text password to hash
+            
+        Returns:
+            str: password_hash
+        """
+
+        salt = os.getenv('PASSWORD_SALT')
+
+        if isinstance(salt, str):
+            salt = salt.encode('utf-8')
+            
+        password_bytes = password.encode('utf-8')
+        hash_obj = hashlib.sha256()
+        hash_obj.update(salt)
+        hash_obj.update(password_bytes)
+        password_hash = hash_obj.hexdigest()
+        
+        return password_hash
+
+    @staticmethod
     def validate(user):
-        """Check if a user exists in SQLite database.
+        """Check if required fields are provided.
 
         Args:
             user ([type]): User object.
 
         Returns:
-            bool: True, if uuid and username are found in SQLite database.
+            bool: True, if uuid, username and password exists.
         """
-        if user.get("uuid") and user.get("user_name"):
+        if (user.get("uuid") and user.get("user_name") and user.get("password")):
             return True
         else:
             return False
@@ -100,7 +125,13 @@ class DB:
             response: True, if user was created successfully in SQLite database.
         """
         try:
-            DB.UserModel.create(uuid=user["uuid"], user_name=user["user_name"])
+            password_hash = DB.UserModel.hash_password(user["password"])
+            DB.UserModel.create(
+                uuid=user["uuid"],
+                user_name=user["user_name"],
+                password_hash=password_hash,
+                is_admin= user["is_admin"]
+            )
             return response(True)
         except Exception as e:
             # TODO: log exceptions
@@ -132,6 +163,47 @@ class DB:
             return response(
                 False,
                 message="user %s does not exist" % uuid
+            )
+        
+    @staticmethod
+    def check_password(user: dict):
+        """Check if provided username and password matches.
+
+        Args:
+            user ([type]): User object.
+
+        Returns:
+            bool: True, if username and password are a match in SQLite database.
+        """
+        try:
+            if not (user.get("user_name") and user.get("password")):
+                return response(
+                    False,
+                    message="Provide username and password"
+                )
+                
+            db_user = UserModel.get(
+                UserModel.user_name == user["user_name"]
+            )
+            
+            provided_hash = UserModel.hash_password(user["password"])
+            
+            if provided_hash == db_user.password_hash:
+                data = {
+                    "uuid": db_user.uuid,
+                    "user_name": db_user.user_name,
+                    "is_admin": db_user.is_admin,
+                }
+                return response(True, data=data)
+            return response(
+                False,
+                message="Provided credentials to not match"
+            )
+            
+        except DoesNotExist:
+            return response(
+                False,
+                message="Provided credentials to not match"
             )
 
     @staticmethod
